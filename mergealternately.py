75a71d3977c5039755f391e56716eabc148d29dd
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-01 22:33:09 +0800

    Add files via upload

---
+++ mergealternately.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0
+        j = 0
+        answer = []
+        while i < len(word1) and j < len(word2):
+            answer += word1[i]
+            answer += word2[j]
+            i+=1
+            j+=1
+        if i == len(word1):
+            answer += word2[j:]
+        else:
+            answer += word1[i:]
+
+        return "".join(answer)
+        
