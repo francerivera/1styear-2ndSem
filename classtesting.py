6b27636fc91053e9036524256e45805213c5d7b8
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-05 23:31:08 +0800

    Update classtesting.py

--- classtesting.py
+++ classtesting.py
@@ -18,3 +18,27 @@ def validate_info() -> None:
         print("Student information is not valid.")
 
 validate_info()
+
+or
+
+class student:
+    def __init__(self,id_number: int,name: str,course: str) -> str:
+        self.id_number = id_number
+        self.name = name
+        self.course = course
+        
+id_number = "123456789"
+name = "John Doe"   
+course = "Comsci"
+
+def __str__() -> str:
+    print(f"Testing Student: {id_number} - {name} - {course}")
+        
+def validate_info() -> None:
+    if len(id_number) == 9 and name.isalpha:
+        print ("Student information is valid.")
+    else:
+        print("Student information is not valid.")
+
+__str__()
+validate_info()
