9ea38526b57871b2b881a059801453ba84d9cca6
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-07 17:17:16 +0800

    Update classtesting.py

--- classtesting.py
+++ classtesting.py
@@ -1,17 +1,22 @@
 import datetime
 import pytz
-class student:
+class Student:
     def __init__(self, id_number: int, name: str, course: str):
         self.id_number = id_number
         self.name = name
         self.course = course
-    def __str__(self):
-        return f"Testing Student: {id_number} - {name} - {course}"
-    def validate_info(self):
-        if len(id_number) == 9 and name.replace(' ','').isalpha():
+    def __str__(self) -> str:
+        return f"{self.id_number} - {self.self.name} - {self.course}"
+    def validate_info(self) -> None:
+        if len(str(self.id_number)) == 9 and self.name.replace(' ','').isalpha():
             print("Student information is valid.")
         else:
             print("Student information is not valid.")
 
 user = input("Testing Student: ").split(" - ")
-profile = student(int(user[0]), user[1], user[2])
+profile = Student(int(user[0]), user[1], user[2])
+profile.validate_info()
+
+ph_timezone = pytz.timezone('Asia/Manila')
+time_in_ph = datetime.datetime.now(ph_timezone)
+print(time_in_ph.strftime(format = "%Y-%m-%d %H:%M"))
