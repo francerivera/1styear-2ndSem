7de4b3d878df28d362804287f77948e209bcec40
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-07 16:28:43 +0800

    Update classtesting.py

--- classtesting.py
+++ classtesting.py
@@ -1,45 +1,17 @@
+import datetime
+import pytz
 class student:
-    def __init__(self,id_number: int,name: str,course: str) -> str:
+    def __init__(self, id_number: int, name: str, course: str):
         self.id_number = id_number
         self.name = name
         self.course = course
-
-    def __str__() -> str:
-        return f"Testing Student: {id_number} - {name} - {course}")
-        
-    def validate_info() -> None:
-        if len(id_number) == 9 and name.isalpha():
-            print ("Student information is valid.")
+    def __str__(self):
+        return f"Testing Student: {id_number} - {name} - {course}"
+    def validate_info(self):
+        if len(id_number) == 9 and name.replace(' ','').isalpha():
+            print("Student information is valid.")
         else:
             print("Student information is not valid.")
 
-id_number = input("ID Number: ")
-name = str(input("Name: "))   
-course = str(input("Course: "))
-
-__str__()
-validate_info()
-
-or
-
-class student:
-    def __init__(self,id_number: int,name: str,course: str) -> str:
-        self.id_number = id_number
-        self.name = name
-        self.course = course
-        
-id_number = "123456789"
-name = "John Doe"   
-course = "Comsci"
-
-def __str__() -> str:
-    print(f"Testing Student: {id_number} - {name} - {course}")
-        
-def validate_info() -> None:
-    if len(id_number) == 9 and name.isalpha():
-        print ("Student information is valid.")
-    else:
-        print("Student information is not valid.")
-
-__str__()
-validate_info()
+user = input("Testing Student: ").split(" - ")
+profile = student(int(user[0]), user[1], user[2])
