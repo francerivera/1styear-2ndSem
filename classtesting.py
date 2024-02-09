9c532b3062bc885f2857aed6d9a77765a219c6ce
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-09 16:00:15 +0800

    Update classtesting.py

--- classtesting.py
+++ classtesting.py
@@ -5,18 +5,23 @@ class Student:
         self.id_number = id_number
         self.name = name
         self.course = course
+        
     def __str__(self) -> str:
         return f"{self.id_number} - {self.self.name} - {self.course}"
+        
     def validate_info(self) -> None:
         if len(str(self.id_number)) == 9 and self.name.replace(' ','').isalpha():
             print("Student information is valid.")
         else:
             print("Student information is not valid.")
 
-user = input("Testing Student: ").split(" - ")
-profile = Student(int(user[0]), user[1], user[2])
-profile.validate_info()
+try:
+    user = input("Testing Student: ").split(" - ")
+    profile = Student(int(user[0]), user[1], user[2])
+    profile.validate_info()
 
-ph_timezone = pytz.timezone('Asia/Manila')
-time_in_ph = datetime.datetime.now(ph_timezone)
-print(time_in_ph.strftime(format = "%B %d, %Y  %H:%M"))
+    ph_timezone = pytz.timezone('Asia/Manila')
+    time_in_ph = datetime.datetime.now(ph_timezone)
+    print(time_in_ph.strftime(format = "%B %d, %Y  %H:%M"))
+except:
+    print("Please follow the format.")
