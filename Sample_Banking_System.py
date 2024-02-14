e3516bcd97a064824a2bdc6b135ba38920dce5c5
France Raphael RIvera <francerivera1231@gmail.com>
2024-02-14 23:59:25 +0800

    Update Experiment_Banking.py

--- Sample_Banking_System.py
+++ Sample_Banking_System.py
@@ -1,94 +1,95 @@
 import random
 import datetime
 import pytz
-import time
+
 class BankAccount:
-    def __init__(self, account_number, account_holder, balance=0):
+    total_deposits = 0
+    total_withdraws = 0
+    
+    def __init__(self, account_number, account_holder, balance = 0):
         self.account_number = account_number
         self.account_holder = account_holder
         self.balance = balance
-    def deposit(self,amount):
+
+    def deposit(self, amount):
         self.balance += amount
-    def withdraw(self,amount):
-        if self.balance - amount < 0:
-            print(currenttime.strftime("%B %d %Y (%A)"), currenttime.strftime("%I: %M: %S %p"))
-            print("Transaction Failed: Not Enough Funds.")
+        self.total_deposits += 1 
+        return f"Your current balance is now: ${round(self.balance, 2)}"
+        
+    def withdraw(self, amount): 
+        if amount > self.balance: 
+            print(time_in_ph.strftime(format = "%B %d, %Y  |  %H:%M"))
+            print("Insufficient funds | Balance available: $", self.balance)
         else:
-            print(currenttime.strftime("%B %d %Y (%A)"), currenttime.strftime("%I: %M: %S %p"))
-            print(f"Withdrawal of ${amount:.2f} successful.")
-            self.balance -= amount
-            print (f"Current Balance: ${self.get_balance()}\n")
+            self.balance -= amount 
+            self.total_withdraws += 1
+            print(time_in_ph.strftime(format = "%B %d, %Y  |  %H:%M"))
+            print(f"Withdrawal of {amount} successful. Current Balance: $", self.balance) 
+    
     def get_balance(self):
-        return format(self.balance,".2f")
+        return format(self.balance, ".2f")
+        
     def display_info(self):
-        print(currenttime.strftime("%B %d %Y (%A)"), currenttime.strftime("%I: %M: %S %p"))
+        print(time_in_ph.strftime(format = "%B %d, %Y  |  %H:%M"))
         print(f"Account Number: {self.account_number}")
         print(f"Account Holder: {self.account_holder}")
         print(f"Balance: ${self.get_balance()}")
 
+bank_accs = {}
 
-currenttime = datetime.datetime.now(pytz.timezone("Asia/Manila"))
-print(currenttime.strftime("%B %d %Y (%A)"), currenttime.strftime("%I: %M: %S %p"))
-bankAccs = {
-
-}
 while True:
-    type_of_user = input("Log in or Sign up? ").lower()
-    if type_of_user == "log in" or type_of_user == "login":
-        account_id = input("Please type your account ID: ")
-        if account_id in list(bankAccs.keys()):
-            print(f"Welcome {bankAccs[account_id].account_holder}!")
+    ph_timezone = pytz.timezone('Asia/Manila')
+    time_in_ph = datetime.datetime.now(ph_timezone)
+    print(time_in_ph.strftime(format = "%B %d, %Y  |  %H:%M"))  
+    print("Welcome to Rivera Bank")
+    new_user = input("Would you like to register a new account?\nType 'Yes' to create your bank | 'No' if you already have an account: ")
+    if new_user.lower() == 'yes':
+        account_holder = input("Please enter your name: ")
+        account_number = random.randint(1,9999)
+        while account_number in bank_accs: 
+            account_number = random.randint(1,9999)
+        bank_accs[account_number] = BankAccount(account_number, account_holder) 
+        print("Account Number:", account_number)
+        print("Account Holder:", account_holder.title())  
+    elif new_user.lower() == 'no':
+        account_pass = int(input("Please type your account number: ")) 
+        if account_pass in bank_accs:
             while True:
-                print("**- Deposit, Withdraw, Balance, Exit -**")
-                activity = input("What activity would you like to do for today? ").lower()
-                if "deposit" in activity:
-                    while True:
-                        try:
-                            amount = int(input("Enter the amount you want to deposit: "))
-                            bankAccs[account_id].deposit(amount)
-                            print(currenttime.strftime("%B %d %Y (%A)"), currenttime.strftime("%I: %M: %S %p"))
-                            print(f"${amount} is successfully added to your balance.\n")
-                            break
-                        except:
-                            print("Invalid input. Please only enter a valid amount.")
-                            continue
-                elif "withdraw" in activity:
-                    while True:
-                        try:
-                            amount = int(input("Enter the amount you want to withdraw: "))
-                            if bankAccs[account_id].balance - amount < 0:
-                                print("Transaction failed. Going back to main menu.")
-                                break
-                            else:
-                                bankAccs[account_id].withdraw(amount)
-                                break
-                        except:
-                            print("Invalid input. Please only enter a valid amount.")
-                            continue
-                elif "balance" in activity:
-                    bankAccs[account_id].display_info()
+                process = input("Here are the lists of options:\n(1) Deposit\n(2) Withdraw\n(3) Balance\n(4) Exit\nPlease choose the number you want: ")
+                if process == '1':
+                    try:
+                        amount = float(input("Enter the amount you want to deposit: ")) 
+                        bank_accs[account_pass].deposit(amount)
+                        print(time_in_ph.strftime(format = "%B %d, %Y  |  %H:%M"))
+                        print(f"${amount} is successfully added to your balance.\n")
+                        break
+                    except ValueError:
+                        print("Invalid input. Please only enter a valid amount.")
+                        continue
+                elif process == '2':
+                    try:
+                        amount = float(input("Enter the amount you want to withdraw: ")) 
+                        bank_accs[account_pass].withdraw(amount)
+                        break
+                    except ValueError:
+                        print("Invalid input. Please only enter a valid amount.")
+                        continue
+                elif process == '3':    
+                    bank_accs[account_pass].display_info()
                     continue
-                elif "quit" in activity or "exit" in activity:
+                elif process == '4':
                     break
                 else:
-                    print("Invalid input. Please choose only between deposit, withdraw or exit/quit.")
+                    print("Invalid input. Please choose only the numbers in the options.")
         else:
-            print("Account not found")
-    elif type_of_user == "signup" or type_of_user == "sign up":
-        account_name = input("Please enter your name: ")
-        account_no = random.randint(1,99999)
-        while True:
-            for num in range(len(bankAccs)):
-                if account_no in list(bankAccs.keys())[num]:
-                    account_no = random.randint(1,99999)
-            else:
-                break
-        bankAccs[f"{account_no}_{account_name}"] = BankAccount(account_no,account_name)
-        print(f"Your account id will be: {account_no}_{account_name}")
-        print("Please Remember it.\n")
-    elif "quit" in type_of_user or "exit" in type_of_user:
-        print("Thank you for using our program! Terminating...")
-        time.sleep(2)
-        break
-else:
-    print("Invalid input, Please Try Again.")
+            print("Account not found.")
+    else:
+        print("Invalid input. Please Try Again.")
+
+    
+    
+
+        
+        
+        
+        
