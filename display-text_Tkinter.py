61176eb898873425f6a8527978b4aa517181786f
France Raphael RIvera <francerivera1231@gmail.com>
2024-03-08 17:09:03 +0800

    Update display-text_Tkinter.py

--- display-text_Tkinter.py
+++ display-text_Tkinter.py
@@ -1,81 +1,48 @@
 from tkinter import *
 
-root = Tk()
-root.title("J1T-midLA1")
-root.geometry('500x200')
-
 def GUI():
     def display():
-        value = E1.get()
-        msg.config(text=value)
+        word = entry_text.get()
+        if len(word.split()) > 1:
+            label_text.config(text="One word only.", fg='red')
+        else:
+            label_text.config(text=word, wraplength=250, fg='black')
+
+    root = Tk()
+    root.title("J1T-midLA1")
+    root.geometry('700x400+700+400')
+    root.configure(bg='#B8DE8F')
 
 
     frame1 = Frame(root)
-    frame1.pack(anchor=N)
-    L1 = Label(frame1, text="France Raphael R. RIvera", padx=10)
-    L1.pack()
-    L2 = Label(frame1, text="Input Text Here", padx=10)
-    L2.pack()
+    frame1.pack(anchor=N, pady=10)
+    name_text = Label(frame1, text="France Raphael R. Rivera", font=('Monotype Corsiva', 50, 'bold')
+                      ,bg='#800000', fg='gold', bd=12, relief=FLAT)
+    name_text.pack()
 
     frame2 = Frame(root)
-    frame2.pack(padx=10, pady=10, anchor=N)
-    E1 = Entry(frame2, bd=5, relief=SUNKEN)
-    E1.pack()
+    frame2.pack()
+    input_text = Label(frame2, text="Input Text Here:", font=('Rockwell', 20, 'bold')
+                       ,bg='#800000', fg='white', bd=8, relief=RIDGE)
+    input_text.pack()
 
     frame3 = Frame(root)
-    frame3.pack(side=TOP)
-    b1 = Button(frame3, text='Display Text',command=display)
-    b1.pack()
+    frame3.pack(anchor=N, pady=10)
+    entry_text = Entry(frame3, font=('Helvetica', 20), bd=5, relief=SUNKEN, width=36)
+    entry_text.pack()
 
     frame4 = Frame(root)
-    frame4.pack(anchor=N)
-    L3 = Label(frame4, text = "Display", padx= 10, pady=10)
-    L3.pack()
+    frame4.pack(side=TOP, pady=5)
+    button_display = Button(frame4, text='Display Text',font=('Arial', 15, 'bold')
+                ,bg='#008080', fg='white', bd=8, command=display)
+    button_display.pack()
 
     frame5 = Frame(root)
-    frame5.pack(anchor=S)
-    msg = Label(frame5, text=" ", wraplength=300, padx= 10, pady=10)
-    msg.pack()
+    frame5.pack(anchor=S, pady=10)
+    label_text = Label(frame5, bg='#B8DE8F', padx= 10, pady=10,font=('Helvetica', 25, 'bold'))
+    label_text.pack()
 
-
+    root.mainloop()
 
 GUI()
-root.mainloop()
 
-
-****************************
-
-    
-import tkinter as tk
-from tkinter import ttk
-import re
-
-
-class MainWindows(tk.Tk):
-
-    def __init__(self):
-        super().__init__()
-
-        self.frame_main = ttk.Frame(self)
-
-        self.button_display = ttk.Button(self.frame_main, text="Display Text")
-
-        self.label_text = ttk.Label(self.frame_main, text="Hello!")
-
-
-
-        self.frame_main.pack(fill=tk.BOTH, expand=True)
-        self.button_display.pack()
-        self.label_text.pack()
-
-
-    def get_words_btn(self):
-        pass
-
-
-
-
-
-if __name__ == "__main__":
-    main_window = MainWindows()
-    main_window.mainloop()
