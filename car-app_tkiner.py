6bf70883c84d8fb78972156834e23aa53d3c8e18
France Raphael RIvera <francerivera1231@gmail.com>
2024-03-16 00:50:13 +0800

    Update car-app_tkiner.py

--- car-app_tkiner.py
+++ car-app_tkiner.py
@@ -5,6 +5,7 @@ def GUI():
     root = Tk()
     root.title("J1T-midLA2")
     root.geometry("700x300")
+    root.configure(bg='#ffa100')
 
     name_text = Label(root, text="Rivera's Car Maintenance App", font=('Arial', 25, 'bold'),
                         bg='#48D5E7', fg='white', bd=12, relief=FLAT)
@@ -25,8 +26,8 @@ def GUI():
     interior.append(in_var1)
     interior.append(in_var2)
 
-    values2 = {"Leather   550.00": 550,
-               "GPS         1000.00": 1000}
+    values2 = {"Leather                                                                    550.00": 550,
+               "GPS         1,000.00": 1000}
 
     exterior_f = StringVar()
     exterior_f.set(None)
@@ -41,8 +42,8 @@ def GUI():
     exterior.append(ex_var1)
     exterior.append(ex_var2)
 
-    values4 = {"Wheel Upgrade                  550.00": 550,
-               "Performance Package      1000.00": 1000}
+    values4 = {"Wheel Upgrade                                                      550.00": 550,
+               "Performance Package                                  1,000.00": 1000}
 
     ParentFrame=Frame(root)
     ParentFrame.pack(pady=20)
@@ -55,13 +56,13 @@ def GUI():
         radio_button.grid(row=index+1, column=0, padx=10)
 
 
-    Label(CarFrame,text="Car Brand", font=("Arial", 10, 'bold')).grid(row=0, column=0)
+    Label(CarFrame, text="Car Brand", font=("Arial", 10, 'bold')).grid(row=0, column=0)
     Label(CarFrame, text="Interior Option", font=("Arial", 10, 'bold')).grid(row=0, column=1)
     Label(CarFrame, text="Exterior Finish", font=("Arial", 10, 'bold')).grid(row=0, column=2)
     Label(CarFrame, text="Exterior Option", font=("Arial", 10, 'bold')).grid(row=0, column=3)
     check_button1 = Checkbutton(CarFrame, text="Leather       550.00", variable=in_var1, onvalue=550, offvalue=0)
     check_button1.grid(row=1, column=1, padx=10, sticky='W')
-    check_button2 = Checkbutton(CarFrame, text="GPS            1000.00", variable=in_var2, onvalue=1000, offvalue=0)
+    check_button2 = Checkbutton(CarFrame, text="GPS            1,000.00", variable=in_var2, onvalue=1000, offvalue=0)
     check_button2.grid(row=2, column=1, padx=10, sticky='W')
 
     for index, item in enumerate(values3):
@@ -70,46 +71,50 @@ def GUI():
         radio_button.grid(row=index+1, column=2, padx=10, sticky='W')
 
 
-    check_button1 = Checkbutton(CarFrame, text="Wheel Upgrade                  550.00", variable=ex_var1, onvalue=550, offvalue=0)
+    check_button1 = Checkbutton(CarFrame, text="Wheel Upgrade                    550.00", variable=ex_var1, onvalue=550, offvalue=0)
     check_button1.grid(row=1, column=3, padx=10, sticky='W')
-    check_button2 = Checkbutton(CarFrame, text="Performance Package      1000.00", variable=ex_var2, onvalue=1000, offvalue=0)
+    check_button2 = Checkbutton(CarFrame, text="Performance Package      1,000.00", variable=ex_var2, onvalue=1000, offvalue=0)
     check_button2.grid(row=2, column=3, padx=10, sticky='W')
 
 
     def totality():
         top = Toplevel()
+        top.geometry("400x300")
+        top.title("Receipt")
         new_interior = list(filter(0 or None, [item.get() for item in interior]))
         new_exterior = list(filter(0 or None, [item.get() for item in exterior]))
         print(new_exterior)
         print(new_interior)
         total_interior = sum(new_interior[i] for i in range(len(new_interior)))
         total_exterior = sum(new_exterior[i] for i in range(len(new_exterior)))
-        car_cost = car.get()
+        car.get()
         total = (int(values[car.get()]) + total_interior + total_exterior)
-        car_type= Label(top, text=f"You selected: {car.get()}").pack(anchor=W)
+        Label(top, text=f"You selected: {car.get()}",padx=10,pady=10).pack(anchor=W)
         if len(new_interior) > 1:
-            interior_option = Label(top, text=f"You selected:  {list(values2.keys())[list(values2.values()).index(new_interior[0])]} and {list(values2.keys())[list(values2.values()).index(new_interior[1])]}").pack(anchor=W)
+            interior_text = f"You selected: {list(values2.keys())[list(values2.values()).index(new_interior[0])]} and {list(values2.keys())[list(values2.values()).index(new_interior[1])]}"
         elif len(new_interior) == 1:
-            interior_option = Label(top, text=f"You selected:  {list(values2.keys())[list(values2.values()).index(new_interior[0])]}")
-            interior_option.pack(anchor=W)
+            interior_text = f"You selected: {list(values2.keys())[list(values2.values()).index(new_interior[0])]}"
         else:
-            interior_option = Label(top, text=f"You selected: N/A")
-            interior_option.pack(anchor=W)
+            interior_text = "You selected: N/A"
+        Label(top, text=interior_text, padx=10,pady=10).pack(anchor=W)
 
-        if exterior_f == None:
-            exterior_finish = Label(top, text=f"You selected: N/A").pack(anchor=W)
+        if exterior_f.get():
+            exterior_finish_text = f"You selected: {exterior_f.get()}"
         else:
-            exterior_finish = Label(top, text= f"You selected:  {exterior_f.get()}").pack(anchor=W)
+            exterior_finish_text = "You selected: N/A"
+        Label(top, text=exterior_finish_text, padx=10,pady=10).pack(anchor=W)
+
         if len(new_exterior) > 1:
-            exterior_option = Label(top, text=f"You selected:  {list(values4.keys())[list(values4.values()).index(new_exterior[0])]} and {list(values4.keys())[list(values4.values()).index(new_exterior[1])]}").pack(anchor=W)
+            exterior_option_text = f"You selected: {list(values4.keys())[list(values4.values()).index(new_exterior[0])]}\n{list(values4.keys())[list(values4.values()).index(new_exterior[1])]}"
         elif len(new_exterior) == 1:
-            exterior_option = Label(top, text=f"You selected:  {list(values4.keys())[list(values4.values()).index(new_exterior[0])]}").pack(anchor=W)
+            exterior_option_text = f"You selected: {list(values4.keys())[list(values4.values()).index(new_exterior[0])]}"
         else:
-            exterior_option = Label(top, text=f"You selected: N/A").pack(anchor=W)
-        total_label = Label(top, text= f"Total Amount: {total}").pack(anchor=E)
+            exterior_option_text = "You selected: N/A"
+        Label(top, text=exterior_option_text, padx=10,pady=10).pack(anchor=W)
+        Label(top, text="________________________________________________________________________").pack()
+        Label(top, text=f"Total Amount: {total}", font=('Arial', 15, 'bold'),padx=30,pady=10).pack(anchor=E)
 
-
-    confirm = Button(root, text="Purchase", command= totality)
+    confirm = Button(root, text="Purchase", font=('Arial', 15, 'bold'), bd=8, relief=GROOVE, command= totality)
     confirm.pack()
 
 
