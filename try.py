069632f83590b465b75c018b8e44dc1617cb9e5d
France Raphael RIvera <francerivera1231@gmail.com>
2024-04-01 22:36:40 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,1 +1,31 @@
-asdasd
+from tkinter import *
+
+window = Tk()
+
+def increase():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value + 1}"
+
+def decrease():
+    value = int(lbl_value["text"])
+    if value > 0:
+        lbl_value["text"] = f"{value - 1}"
+
+
+
+btn_decrease = Button(master=window, text="-", command=decrease)
+btn_decrease.grid(row=0, column=1, sticky="nsew")
+
+
+lbl_value = Label(master=window, text="0")
+lbl_value.grid(row=0, column=2)
+
+btn_increase = Button(master=window, text="+", command=increase)
+btn_increase.grid(row=0, column=3, sticky="nsew")
+
+window.mainloop()
+
+##############
+
+
+
